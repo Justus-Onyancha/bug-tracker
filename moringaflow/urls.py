bfd9351d588b32dad49e93a97182eb4109f0a68a
Justus-Onyancha <justus.onyancha@moringaschoool.com>
2021-10-06 11:07:21 +0300

    first commit

---
+++ moringaflow/urls.py
@@ -0,0 +1,30 @@
+"""moringaflow URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def bugs(request):
+    return HttpResponse("This is a bug page")
+
+def bug(request):
+    return HttpResponse("single bug")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bugs/',bugs, name="bugs"),
+    path('bug/',bugs, name="bug")
+]
