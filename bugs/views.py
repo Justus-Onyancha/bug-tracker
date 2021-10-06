60e4a1db7f36f07ac5e59a293bf11516638fcd92
Justus-Onyancha <justus.onyancha@moringaschoool.com>
2021-10-07 01:36:48 +0300

    added views and urls

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def bugs(request):
+    return render(request, 'bugs.html')
+
+def bug(request, pk):
+    return render(request, 'single-bug.html')
